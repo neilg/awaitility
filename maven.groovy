ae65f02348bf8d5f91a0caf6cc3e53b1ac82ccbe
Neil Green <ngreen@quantbet.com>
2019-09-30 13:34:38 +0100

    Tweaks for testing

--- maven.groovy
+++ maven.groovy
@@ -1,13 +1,13 @@
 podTemplate(containers: [
-  containerTemplate(name: 'maven', image: 'maven:3.6.2-jdk-8-slim', ttyEnabled: true, command: 'cat')
-  ]) {
+        containerTemplate(name: 'maven', image: 'maven:3.6.2-jdk-8-slim', ttyEnabled: true, command: 'cat')
+]) {
 
-  node(POD_LABEL) {
-    stage('Build a Maven project') {
-      git 'https://github.com/neilg/awaitility.git'
-      container('maven') {
-          sh 'mvn -B clean package'
-      }
+    node(POD_LABEL) {
+        stage('Build a Maven project') {
+            git 'https://github.com/neilg/awaitility.git'
+            container('maven') {
+                sh 'mvn -B clean verify'
+            }
+        }
     }
-  }
 }
