b42efc92494e80ef391160fb4017ed1ad34a57cb
Neil Green <ngreen@quantbet.com>
2019-09-30 13:20:15 +0100

    Use correct git url

--- maven.groovy
+++ maven.groovy
@@ -4,7 +4,7 @@ podTemplate(containers: [
 
   node(POD_LABEL) {
     stage('Build a Maven project') {
-      git 'https://github.com/jenkinsci/kubernetes-plugin.git'
+      git 'https://github.com/neilg/awaitility.git'
       container('maven') {
           sh 'mvn -B clean package'
       }
